c464a31d319c170ef7c848ad887af2674c3a80fa
Nick <nicholas.kundu@gmail.com>
2015-09-08 03:32:41 +0000

    preamble and write byte fix

--- blink.c
+++ blink.c
@@ -7,53 +7,60 @@
 #include "manchester.h"
 
 
-#define	LED	(0)
+#define	PIN_W	(0)
+#define PIN_R   (1)
 #define DELAY   (20)
+#define BUFSZ   (10)
 
-char read = 0;
-void myInterrupt1(void) { read = read | 0x01; };
+unsigned int read = 0;
+void myInterrupt1(void) { read = millis() - read; };
 
 PI_THREAD  (readClock)
 {
+  //char buf[BUFSZ];
+  //int result;
+  //result = manchesterRead(DELAY, &(buf[0]), BUFSZ, &read, delay);
   int i;
   for (i = 0;;i++)
   {
-    delay(DELAY*3);
-    read <<= 1;
-    if (i == 7)
-    {
-      i = 0;
-      printf("Read 0x%08X\n",read);
-    }
+    delay(DELAY/2);
+    printf("read %d ", read);
   }
 }
 
 void setLow()
 {
-  digitalWrite(LED, LOW);
+  digitalWrite(PIN_W, LOW);
 }
 
 void setHigh()
 {
-  digitalWrite(LED, HIGH);
+  digitalWrite(PIN_W, HIGH);
 }
 
 int main (void)
 {
-  char c;
+  int c;
   int i;
   printf ("Raspberry Pi slow radio link\n") ;
 
   wiringPiSetup () ;
   i = piThreadCreate(readClock);
-  wiringPiISR(1, INT_EDGE_FALLING, &myInterrupt1);
-  pinMode (LED, OUTPUT) ;
+  wiringPiISR(PIN_R, INT_EDGE_BOTH, &myInterrupt1);
+  pinMode (PIN_W, OUTPUT) ;
 
+  printf("write preamble\n");
+  manchesterWritePreamble(DELAY, delay, setLow, setHigh);
+  printf("write data\n");
   for (;;)
   {
     c = getchar();
-    manchesterWrite(c, DELAY, delay, setLow, setHigh);
+    if (EOF == c)
+      break;
+    manchesterWrite((char)c, DELAY, delay, setLow, setHigh);
     printf ("wrote 0x%08X\n", c);
   }
+
+  printf("done\n");
   return 0 ;
 }
--- manchester.h
+++ manchester.h
@@ -1,1 +1,5 @@
+
+int manchesterRead(int T, char *buf, int bufsz, int *counts, void (*delay)(unsigned int));
+
+void manchesterWritePreamble(int T, void (*delay)(unsigned int),  void (*setLow)(void), void (*setHigh)(void));
 void manchesterWrite(char c, int T, void (*delay)(unsigned int),  void (*setLow)(void), void (*setHigh)(void));
