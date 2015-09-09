b509b2d23f3ea0d81f2457a9af25d1eaf1ea1c01
Nick <nicholas.kundu@gmail.com>
2015-09-09 02:06:19 +0000

    working proto

--- blink.c
+++ blink.c
@@ -14,18 +14,76 @@
 
 unsigned int last_read = 0;
 enum decoder_state { START, START1, MID1, START0, MID0 };
+enum event_len { SHORT, LONG };
+enum event_type { PULSE, SPACE };
+
 enum decoder_state current_state = START;
+
+int count = 0;
+char buf = 0x00;
+void emit(char c)
+{
+	buf <<= 1;
+	buf |= (0x01 & c);
+	count++;
+	if (count == 8)
+	{
+		putchar(buf);
+		fflush(stdout);
+		count = 0;
+		buf = 0x00;
+	}
+}
+
 void myInterrupt1(void) 
 {
   unsigned int now = millis();
-  int pin_state;
-  int read;
-  read = now - last_read;
-  last_read = now;
-  pin_state = digitalRead(PIN_R);
+  int read_ticks;
+  enum event_type type;
+  enum event_len length;
 
-  
-  printf("read %d state %d ", read < 1.5*DELAY ? 1 : 0, pin_state);
+  read_ticks = now - last_read;
+  last_read = now;
+
+  type = digitalRead(PIN_R) == HIGH ? SPACE : PULSE;
+
+
+  if (read_ticks < 0.5 * DELAY)
+  {
+    return;
+  }
+  else if (read_ticks > 0.5 * DELAY && read_ticks < 1.5 * DELAY)
+  {
+    length = SHORT;
+  }
+  else
+  {
+    length = LONG;
+  }
+
+  switch (current_state)
+  {
+  case START:
+    emit(1);
+    current_state = MID1;
+    break;
+  case START1:
+    if (length == SHORT && type == SPACE) {emit(1); current_state = MID1; }
+    break;
+  case MID1:
+    if (length == SHORT && type == PULSE) { current_state = START1; }
+    else if (length == LONG && type == PULSE) { emit(0); current_state = MID0; }
+    break;
+  case START0:
+    if (length == SHORT && type == PULSE) { emit(0); current_state = MID0; }
+    break;
+  case MID0:
+    if (length == SHORT && type == SPACE) { current_state = START0; }
+    else if (length == LONG && type == SPACE) { emit(1); current_state = MID1; }
+    break;
+  default:
+    current_state = START;
+  }
 };
 
 void setLow()
@@ -48,16 +106,16 @@ int main (void)
   wiringPiISR(PIN_R, INT_EDGE_BOTH, &myInterrupt1);
   pinMode (PIN_W, OUTPUT) ;
 
-  printf("write preamble\n");
+  //printf("write preamble\n");
   manchesterWritePreamble(DELAY, delay, setLow, setHigh);
-  printf("write data\n");
+  //printf("write data\n");
   for (;;)
   {
     c = getchar();
     if (EOF == c)
       break;
     manchesterWrite((char)c, DELAY, delay, setLow, setHigh);
-    printf ("wrote 0x%08X\n", c);
+    //printf ("wrote 0x%08X\n", c);
   }
 
   printf("done\n");
---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl --silent http://metaphorpsum.com/sentences/3 | sudo ./blink
